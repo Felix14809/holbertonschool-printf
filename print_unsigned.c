e317f3a1c85955743dc1ab64a4b24070129da38b
Felix Harding <j.felixharding@gmail.com>
2026-08-11 12:12:48 +1000

    string print

--- print_unsigned.c
+++ print_unsigned.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
-  * int_to_usigned - Converts and interger to hexadecimal string
+  * int_to_unsigned - Converts and interger to hexadecimal string
   * @num: Integer to print
-  * 
+  *
   * Return: Number of characters printed
   */
 int int_to_unsigned(unsigned int num)
 {
 	char *str;
 	unsigned int temp, len;
-	
+
 	len = 0;
 	temp = num;
 	if (num == 0)
@@ -31,7 +31,7 @@ int int_to_unsigned(unsigned int num)
 	{
 		temp = num % 10;
 		num /= 10;
-		len--;	
+		len--;
 		str[len] = temp + '0';
 	}
 	while (str[len])
@@ -40,5 +40,4 @@ int int_to_unsigned(unsigned int num)
 		len++;
 	}
 	return (len);
-}	
-
+}
